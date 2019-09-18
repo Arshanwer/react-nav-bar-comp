5d8adf531dbf5e668e1def34058644c2ef7f45f9
Arshad Anwer <arshad_91@live.com>
2019-09-18 13:47:57 +0800

    init commit

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from '@arshanwer/react-nav-bar';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <NavBar>AA</NavBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './mainStyles.scss';
+
+interface Props { }
+
+export const NavBar: React.FC<Props> = () => {
+    return (
+        <div className="header">AAs</div>
+    )
+} 
